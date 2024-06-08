1d943347ef6cb0438d8f1861f210f1132b305314
Daniel Ubanan <du.ubanan@gmail.com>
2024-06-08 11:43:37 +0800

    Initial form design for login and sign-up components

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ButtonComponent, LogoComponent } from '../../../../shared/components';
+import { MatInputModule } from '@angular/material/input';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    LogoComponent,
+    MatInputModule,
+    ButtonComponent,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: '',
+      password: '',
+    });
+  }
+
+  onSubmit(): void {
+    // TODO: call login api
+    console.log(this.form.getRawValue());
+  }
+
+  navToSignUp(): void {
+    this.router.navigateByUrl('auth/sign-up');
+  }
 }
--- src/app/core/auth/pages/sign-up/sign-up.component.ts
+++ src/app/core/auth/pages/sign-up/sign-up.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent, LogoComponent } from '../../../../shared/components';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    LogoComponent,
+    MatInputModule,
+    ButtonComponent,
+  ],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: '',
+      password: '',
+    });
+  }
+
+  onSubmit(): void {
+    // TODO: call register api
+    console.log(this.form.getRawValue());
+  }
 }
